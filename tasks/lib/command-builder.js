9cdb198f25dddeffdad886d1f746b0a0f81d606f
ADS user <ADSUSER@Stephens-MacBook-Pro.local>
2018-04-27 09:56:16 -0500

    bugfixing

--- tasks/lib/command-builder.js
+++ tasks/lib/command-builder.js
@@ -33,7 +33,7 @@ module.exports.mount = function(options, platform, sep){
 
     var path = buildPath(options, platform, sep);
 
-    if(options.hasOwnProperty("mountPoint")){
+    if(options.hasOwnProperty("mountPoint") && options.mountPoint){
       mountPoint = pathNormaliser(options.mountPoint, sep);
     }
 
@@ -75,7 +75,9 @@ module.exports.mount = function(options, platform, sep){
 };
 
 module.exports.unmount = function(options, platform, sep){
-    var mountPoint = pathNormaliser(options.mountPoint, sep);
+    if(options.hasOwnProperty("mountPoint") && options.mountPoint){
+      mountPoint = pathNormaliser(options.mountPoint, sep);
+    }
 
     var command = {
         darwin:[
